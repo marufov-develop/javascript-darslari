a0c178d84a89a235464a05681375f5048a4613fc
Marufov Muhammadamin <themarufov1999@gmail.com>
2025-03-01 15:24:07 +0300

    ehhe

---
+++ 1-dars/script.js
@@ -0,0 +1,13 @@
+		// Console bilan ishlash
+
+		console.log("Salom JavaScript"); // console.log - xabar berish console ga
+		console.error("Xatolik"); // console.error - xatolik console ga
+		console.warn("Ogohlantirish"); // console.warn - ogohlantirish console ga
+
+		// alert bilan ishlash
+
+		// alert("Salom JavaScript")
+
+		// window.confirm("Rostanmi")
+
+		// window.prompt("Ismingizni kiriting");
